471a0ab0a95ea7ad7cf2488ea77cd261c56cc3a7
karelinaas <karelinaas95@gmail.com>
2026-04-23 15:04:10 +0300

    Step 2

--- Makefile
+++ Makefile
@@ -14,3 +14,8 @@ build:
 
 render-start:
 	gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
+
+stop:
+	@lsof -t -i :$(PORT) | xargs kill -9 || true
+
+restart: stop start
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,18 @@
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+
+@app.context_processor
+def inject_now():
+    return {"now": datetime.now()}
+
+@app.route('/')
+def index():
+    return render_template("index.html")
