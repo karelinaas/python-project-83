a55c734ed1f7978c886640a85416a1d38adaa293
karelinaas <karelinaas95@gmail.com>
2026-04-24 14:55:11 +0300

    Step 3 - Fix 404 on render.com

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,20 +3,24 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
-from page_analyzer.routes.urls import urls
+app = Flask(__name__)
+load_dotenv()
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
-def create_app():
-    load_dotenv()
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+def main():
+    register_blueprints()
+    debug: bool = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
+    app.run(debug=debug)
+
+
+def register_blueprints():
+    from page_analyzer.routes.urls import urls
 
     app.register_blueprint(urls)
 
-    return app
-
-
-app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    main()
+else:
+    register_blueprints()
